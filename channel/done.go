13fffd1c46a40d3062739a1b600ed6a85ff7379d
yesterday679 <yesterday679@gmail.com>
2019-02-23 17:19:55 +0800

    channel

---
+++ channel/done.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type worker3 struct {
+	in   chan int
+	done func()
+}
+
+func doWork(id int, w worker3) {
+	for n := range w.in {
+		fmt.Printf("Worker %d received %c\n", id, n)
+		w.done()
+	}
+}
+
+func createWroker(id int, wg *sync.WaitGroup) worker3 {
+	w := worker3{
+		in: make(chan int),
+		done: func() {
+			wg.Done()
+		},
+	}
+	go doWork(id, w)
+	return w
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	var workers [10]worker3
+	for i := 0; i < 10; i++ {
+		workers[i] = createWroker(i, &wg)
+	}
+
+	wg.Add(20)
+	for i, worker := range workers {
+		worker.in <- 'a' + i
+	}
+	for i, worker := range workers {
+		worker.in <- 'A' + i
+	}
+
+	wg.Wait()
+}
