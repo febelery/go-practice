944772b0f7bae2cf1036b5f6773a154cc7b781af
yesterday679 <yesterday679@gmail.com>
2019-02-23 18:33:59 +0800

    queue

---
+++ queue/queue.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Queue []int
+
+func (q *Queue) Push(v int) {
+	*q = append(*q, v)
+}
+
+func (q *Queue) Pop() int {
+	head := (*q)[0]
+	*q = (*q)[1:]
+	return head
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(*q) == 0
+}
+
+func main() {
+	q := Queue{1}
+
+	q.Push(2)
+	q.Push(3)
+
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+}
