a11af3b1f342579685eabfb9a53c837db0b5843f
yesterday679 <yesterday679@gmail.com>
2019-04-08 16:43:18 +0800

    add epoll

---
+++ epoll/evio.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/tidwall/evio"
+
+func main() {
+	var events evio.Events
+	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
+		out = in
+		return
+	}
+
+	if err := evio.Serve(events, "tcp://0.0.0.0:2000"); err != nil {
+		panic(err.Error())
+	}
+}
